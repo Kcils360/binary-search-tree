2590bb1d2964a11c79f2a5ac86226cf6a450a465
Gregory Dukes <kcils360@gmail.com>
2017-10-17 17:02:55 -0700

    made changes to traverse and added Add.  Also completed min and max

--- binary-search-tree/binary-search-tree/BinarySearchTree.cs
+++ binary-search-tree/binary-search-tree/BinarySearchTree.cs
@@ -7,7 +7,7 @@ namespace binarySearchTree
     class BinarySearchTree
     {
         public Node newNode;
-        public Node root;
+
         public BinarySearchTree(Node value)
         {
             newNode = value;
@@ -21,24 +21,76 @@ namespace binarySearchTree
             {
                 if(current.Left != null)
                 {
+                    Console.WriteLine(current.Value);
                     current = current.Left;
                 }
-                else
-                {
-                    current.Left = newNode;
-                }
+
             }
             else
             {
                 if(current.Right != null)
                 {
+                    Console.WriteLine(current.Value);
                     current = current.Right;
+                }
+
+            }
+        }
+        public void Add(Node root, int value)
+        {
+            Node current = root;
+            if (value < current.Value)
+            {
+                if (current.Left == null)
+                {
+                    current.Left = new Node(value);
+                    return;
                 }
                 else
                 {
-                    current.Right = newNode;
+                    Add(current.Left, value);
+                    //return;
                 }
             }
+            if (value > current.Value)
+            {
+                if (current.Right == null)
+                {
+                    current.Right = new Node(value);
+                    return;
+                }
+                else
+                {
+                    Add(current.Right, value);
+                    //return;
+                }
+            }
+        }
+
+        public void Min(Node current)
+        {
+            if(current.Left == null)
+            {
+                Console.WriteLine(current.Value);
+            }
+            else
+            {
+                current.Left = current;
+            }
+            Min(current);
+        }
+
+        public void Max(Node current)
+        {
+            if(current.Right == null)
+            {
+                Console.WriteLine(current.Value);
+            }
+            else
+            {
+                current.Right = current;
+            }
+            Max(current);
         }
     }
 }
--- binary-search-tree/binary-search-tree/Program.cs
+++ binary-search-tree/binary-search-tree/Program.cs
@@ -10,7 +10,16 @@ namespace binary_search_tree
             Node root = new Node(10);
             BinarySearchTree BST = new BinarySearchTree(root);
 
+            BST.Add(root, 7);
+            BST.Add(root, 12);
+            BST.Add(root, 8);
+            BST.Add(root, 3);
+            BST.Add(root, 13);
+            BST.Add(root, 15);
+
             BST.Traversal(root);
+            //BST.Min(root);
+            //BST.Max(root);
 
             Console.Read();
         }
